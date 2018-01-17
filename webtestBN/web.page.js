f4244f294707f1912a64585071381d084dd1eedf
leeb90 <33159848+leeb90@users.noreply.github.com>
2018-01-17 17:41:47 -0600

    Update web.page.js

--- webtestBN/web.page.js
+++ webtestBN/web.page.js
@@ -5,10 +5,21 @@ var WebPage = Object.create(Page, {
      * define elements
      */
    
-    //buttons:      { get: function () { return browser.elements('button[class^="submit"]'); } },
-    buttons:      { get: function () { return browser.elements('button.submit'); } },
+//Sandals
+   	SandalsMenu:      { get: function () { return browser.element('.mm-rsv'); } },
+   	SandalsButtonMenu:      { get: function () { return browser.element('button.submit'); } },
+    SandalsButtonQQ:      { get: function () { return browser.element('div#fullQQ .submit'); } },
+ 
 
+//Beaches
+
+	BeachesMenu:      { get: function () { return browser.element('#book-now'); } },
+   	BeachesButtonMenu:      { get: function () { return browser.element('div.qq-centerer .btn'); } },
+    BeachesButtonQQ:      { get: function () { return browser.element('div.qqFooter .btn'); } },
+
+ //PopUp
+    PopUp:      { get: function () { return browser.element('div[id^="fsrOverlay"]'); } },
+    ClosePopUp:      { get: function () { return browser.element('a[class^="fsrCloseBtn"]'); } },
     
 });
 module.exports = WebPage;
-
